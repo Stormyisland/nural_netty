b010c2ae781b3c354d69cf8eb517d05cfb51d1da
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-07-26 12:26:48 -0700

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -3,4 +3,8 @@ import as np
 class Perceptron:
   def __init(self, input_size, learning_rate=0.10:
     self.weights = np.random.rand(input_size + 1 ) # +1 for bias
-    self.learning_rate 
+    self.learning_rate = learning_rate
+    self error_history = []
+
+  def predict(self, inputs):
+    inputs = np.append(inputs, 1)
